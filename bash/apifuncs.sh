282d44f5f23308f41d7438c328b78698540275bf
cadgo <cadgo.root@gmail.com>
2020-05-06 23:43:50 -0500

    work in progress

--- bash/apifuncs.sh
+++ bash/apifuncs.sh
@@ -24,3 +24,40 @@ function dump_total_hosts(){
 	fi	
 }
 #dumphosts sid.txt hosts.file 150.100.218.18
+function dump_total_hosts2(){
+        sidfile=$1
+        domip=$2
+        dumpfiles=()
+        filenames="hostfile"
+	qs=500
+        totalhosts=0
+	if [ -n $domip ]; then
+		addcli="-d $domip"
+	else
+		addcli=""
+	fi
+	if [ -f "$sidfile" ]; then
+		totalhosts=$(mgmt_cli show hosts --format json -s $sidfile $addcli | jq '.total' | sed -e 's/^"//' -e 's/"$//')
+		iter=0
+		if [ $totalhosts -gt $qs ]; then
+			let ddi=$totalhosts%$qs
+			if [ $ddi -gt 0 ]; then
+				let iter=$totalhosts/$qs
+				let iter=$iter+1
+			else 
+				let iter=$totalhosts/$qs
+			fi
+		else
+			iter=1
+		fi
+		echo "Se tienen un total de $totalhosts hosts dividiendolo en $iter archivos"
+		offset=0
+		for (( x=0; x<$iter; x++ ))
+		do
+			nfile=$filenames${x}
+        	mgmt_cli show hosts limit $qs offset $offset details-level full --format json -s $sidfile $addcli > $nfile
+        	let offset=$offset+$qs
+        	echo "offset $offset qs $qs"
+		done
+	fi
+}
