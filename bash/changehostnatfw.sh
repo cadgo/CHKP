9a26995f35705a25a7aeabd4739db33d6a81e029
cadgo <cadgo.root@gmail.com>
2020-05-08 23:02:31 -0500

    new version for more than 500 objects

--- bash/changehostnatfw.sh
+++ bash/changehostnatfw.sh
@@ -5,64 +5,78 @@ loggerf()
 {
         echo $@ >> $logfile
 }
-
+#PONER FUNCION QUE REMUEVA LOS ARCHIVO HOST.DB Y HSTFILE
 domip=150.100.218.18
 username="admin"
 pass="vpn123"
 if [ -n $domip ]; then
-	mgmt_cli -d $domip login user $username password $pass > sid.txt 	
+	mgmt_cli -d $domip login user $username password $pass > sid.txt
+	addcli="-d $domip"
 else
 	mgmt_cli login user $username password $pass > sid.txt
+	addcli=""
 fi
 #nfw="VS_VoIP"
-nfw="Cluster_VoIP"
-oldfw="VS_VoIP"
-hstfile="hosts.file"
+nfw="VS_VoIP"
+oldfw="Cluster_VoIP"
+hstfile="hostfile"
 hostdb="host_chkp.db"
-loggerf "Creating json database for hosts"
 #dumphosts
-echo "dumphost"
-totalhosts=`cat hosts.file | jq '.total'`
+#totalhosts=`cat hosts.file | jq '.total'`
+#cat hostfile10 | jq '.objects[].name' | wc -l
 loggerf "dumping hosts for looping"
-dump_total_hosts sid.txt $hstfile $domip
-
-if [ -f $hostdb ]; then
-        loggerf "File $hostdb exists creating new one"
-        rm $hostdb
-else
-        loggerf "File didnt exists"
+dump_total_hosts sid.txt $domip
+if [ $? -eq 0 ]; then
+	loggerf "There are not hosts to dump bye bye"
+	exit 0
 fi
-for (( x=0; x<$totalhosts; x++ ))
-do
-        cat $hstfile | jq ".objects[$x].name" >> $hostdb
-done
-r=0
-for xx in $( cat $hostdb )
-do
-        bb=`cat $hstfile | jq ".objects[] | select(.name==$xx)" | jq '."nat-settings"."install-on"' | sed -e 's/^"//' -e 's/"$//'`
-        nattype=`cat $hstfile | jq ".objects[] | select(.name==$xx)" | jq '."nat-settings"."method"' | sed -e 's/^"//' -e 's/"$//'`
-	if [[ "$bb" = "$oldfw" ]]; then
-                if [[ "$nattype" = "static" ]]; then
-                        ipv4=`cat $hstfile | jq ".objects[] | select(.name==$xx)" | jq '."nat-settings"."ipv4-address"' | sed -e 's/^"//' -e 's/"$//'`
-                        auto=`cat $hstfile | jq ".objects[] | select(.name==$xx)" | jq '."nat-settings"."auto-rule"' | sed -e 's/^"//' -e 's/"$//'`
-                        echo "Chaging Fw Nat" $xx " en modo static" >> $logfile
-			loggerf "running mgmt_cli -d $domip set host name $xx nat-settings.auto-rule $auto nat-settings.ip-address $ipv4 nat-settings.method "static" nat-settings.install-on $nfw"
-                        mgmt_cli -d $domip set host name $xx nat-settings.auto-rule $auto nat-settings.ip-address $ipv4 nat-settings.method "static" nat-settings.install-on $nfw -s sid.txt
+for yy in `ls $hstfile*`; do
+	loggerf "creating database $hostdb file for file $yy"
+	totalhosts=$(cat $yy | jq '.objects[].name' | wc -l)
+	loggerf "File $yy has $totalhosts hosts to be iterated"
+	for (( x=0; x<$totalhosts; x++ ))
+	do
+         cat $yy | jq ".objects[$x].name" >> $hostdb
+	done
+	for xx in $( cat $hostdb )
+	do
+         bb=`cat $yy | jq ".objects[] | select(.name==$xx)" | jq '."nat-settings"."install-on"' | sed -e 's/^"//' -e 's/"$//'`
+         nattype=`cat $yy | jq ".objects[] | select(.name==$xx)" | jq '."nat-settings"."method"' | sed -e 's/^"//' -e 's/"$//'`
+ 	if [[ "$bb" = "$oldfw" ]]; then
+             if [[ "$nattype" = "static" ]]; then
+             	ipv4=`cat $yy | jq ".objects[] | select(.name==$xx)" | jq '."nat-settings"."ipv4-address"' | sed -e 's/^"//' -e 's/"$//'`
+                auto=`cat $yy | jq ".objects[] | select(.name==$xx)" | jq '."nat-settings"."auto-rule"' | sed -e 's/^"//' -e 's/"$//'`
+                echo "Chaging Fw Nat" $xx " en modo static" >> $logfile
+ 				loggerf "running mgmt_cli set host name $xx nat-settings.auto-rule $auto nat-settings.ip-address $ipv4 nat-settings.method "static" nat-settings.install-on $nfw $addcli -s sid.txt"
+                mgmt_cli -d $domip set host name $xx nat-settings.auto-rule $auto nat-settings.ip-address $ipv4 nat-settings.method "static" nat-settings.install-on $nfw $addcli -s sid.txt
+            else
+                ipv4=`cat $yy | jq ".objects[] | select(.name==$xx)" | jq '."nat-settings"."ipv4-address"' | sed -e 's/^"//' -e 's/"$//'`
+                auto=`cat $yy | jq ".objects[] | select(.name==$xx)" | jq '."nat-settings"."auto-rule"' | sed -e 's/^"//' -e 's/"$//'`
+                hb=`cat $yy | jq ".objects[] | select(.name==$xx)" | jq '."nat-settings"."hide-behind"' | sed -e 's/^"//' -e 's/"$//'`
+                echo "Changing Fw NAt" $xx " en modo hide" >> $logfile
+                if [[ "$hb" = "gateway" ]]; then
+                	loggerf "mgmt_cli set host name $xx nat-settings.auto-rule $auto nat-settings.hide-behind "gateway" nat-settings.method "hide" nat-settings.install-on $nfw $addcli -s sid.txt"
+                	mgmt_cli set host name $xx nat-settings.auto-rule $auto nat-settings.hide-behind "gateway" nat-settings.method "hide" nat-settings.install-on $nfw $addcli -s sid.txt
                 else
-                        ipv4=`cat $hstfile | jq ".objects[] | select(.name==$xx)" | jq '."nat-settings"."ipv4-address"' | sed -e 's/^"//' -e 's/"$//'`
-                        auto=`cat $hstfile | jq ".objects[] | select(.name==$xx)" | jq '."nat-settings"."auto-rule"' | sed -e 's/^"//' -e 's/"$//'`
-                        echo "Changing Fw NAt" $xx " en modo hide" >> $logfile
-                        loggerf "running mgmt_cli -d $domip set host name $xx nat-settings.auto-rule $auto nat-settings.ip-address $ipv4 nat-settings.method "hide" nat-settings.install-on $nfw"
-			mgmt_cli -d $domip set host name $xx nat-settings.auto-rule $auto nat-settings.ip-address $ipv4 nat-settings.method "hide" nat-settings.install-on $nfw -s sid.txt
-                fi
-	fi
+                	loggerf "running mgmt_cli set host name $xx nat-settings.auto-rule $auto nat-settings.ip-address $ipv4 nat-settings.method "hide" nat-settings.install-on $nfw $addcli -s sid.txt"
+ 					mgmt_cli set host name $xx nat-settings.auto-rule $auto nat-settings.ip-address $ipv4 nat-settings.method "hide" nat-settings.install-on $nfw $addcli -s sid.txt
+             	fi
+             fi
+ 	fi
+	done
+	loggerf "removing $hostdb"
+	rm $hostdb
 done
-if [ -n $domip ]; then
-	loggerf "publicando informacion"
-        mgmt_cli -d $domip publish -s sid.txt
-	mgmt_cli -d $domip logout -s sid.txt
+echo "Do you want to publish [Y/N]"
+read option
+if [ "$option" = "Y" ]; then
+	loggerf "publishing session"
+	mgmt_cli publish $addcli -s sid.txt
+	mgmt_cli logout $addcli -s sid.txt
 else
-	loggerf "publicando informacion"
-	mgmt_cli publish -s sid.txt
-        mgmt_cli logout -s sid.txt
+	loggerf "Dropping session :("
+	mgmt_cli discard $addcli -s sid.txt
+	mgmt_cli logout $addcli -s sid.txt
 fi
+loggerf "Removiendo archivos"
+#remover archivos rutina
