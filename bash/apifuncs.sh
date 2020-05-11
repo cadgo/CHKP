88241f6a3a12182e76b80f378b028857f6c8eb25
cadgo <cadgo.root@gmail.com>
2020-05-11 11:37:42 -0500

    dump total networks

--- bash/apifuncs.sh
+++ bash/apifuncs.sh
@@ -39,3 +39,46 @@ function dump_total_hosts(){
                 return 1
         fi
 }
+
+function dump_total_nets(){
+        sidfile=$1
+        domip=$2
+        filenames="netfiles"
+        qs=500
+        totalnets=0
+        if [ -n $domip ]; then
+                addcli="-d $domip"
+        else
+                addcli=""
+        fi
+        if [ -f "$sidfile" ]; then
+                totalnets=$(mgmt_cli show networks --format json -s $sidfile $addcli | jq '.total' | sed -e 's/^"//' -e 's/"$//')
+                iter=0
+                if [ $totalnets -eq 0 ]; then
+                        echo "There are not networks in this server"
+                        return 0
+                fi
+                if [ $totalnets -gt $qs ]; then
+                        let ddi=$totalnets%$qs
+                        if [ $ddi -gt 0 ]; then
+                                let iter=$totalnets/$qs
+                                let iter=$iter+1
+                        else
+                                let iter=$totalnets/$qs
+                        fi
+                else
+                        iter=1
+                fi
+                echo "Total networks $totalnets splitted by $iter files"
+                offset=0
+                for (( x=0; x<$iter; x++ ))
+                do
+                        nfile=$filenames${x}
+                mgmt_cli show networks limit $qs offset $offset details-level full --format json -s $sidfile $addcli > $nfile
+                let offset=$offset+$qs
+                echo "Generating file $nfile to store from " $((x*qs)) " To $offset"
+                done
+                return 1
+        fi
+}
+
