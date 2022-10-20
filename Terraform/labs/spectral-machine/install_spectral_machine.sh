b53b48bd88a0e492f94bb4b690f85f0db52705ee
Carlos Diaz <cadgo.root@gmail.com>
2022-10-20 18:25:55 -0500

    simple script for installing spectral dependencies for a lab

---
+++ Terraform/labs/spectral-machine/install_spectral_machine.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+logfile="/var/tmp/install_spectral.log"
+SF="git xfce4 xrdp"
+
+echo "updating the system" > $logfile
+sudo apt update
+
+echo "installing $SF" >> $logfile
+sudo apt install -y $SF
+
+echo "enabling xrdp on system" >> $logfile
+sudo systemctl enable xrdp
+sudo adduser xrdp ssl-cert
+echo xfce4-session >~/.xsession
+
+echo "Restarting xrdp for login" >> $logfile
+sudo service xrdp restart
+
+echo "Installing Chrome" >> $logfile
+cd /tmp
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get -y --force-yes install -f
+rm /tmp/google-chrome-stable_current_amd64.deb
+
+mkdir $HOME/git
+cd $HOME/git 
+echo "Clone github Spectral Spectralgoat" >> $logfile
+git clone https://github.com/chkp-cdiaz/SpectralGoat.git
+
+echo "Clone PayloadAllTheThings" >> $logfile
+git clone https://github.com/chkp-cdiaz/PayloadsAllTheThings.git
