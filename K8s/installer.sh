76b72a28a90c169243691f06c5338c9a10afe635
Carlos Diaz <cadgo.root@gmail.com>
2023-01-03 03:50:10 +0000

    common steps

--- K8s/installer.sh
+++ K8s/installer.sh
@@ -5,6 +5,9 @@
 KIND=""
 ID=0
 MASTERIP=""
+USERID=$(id -u)
+CRIO_V=1.24
+CIDR="10.244.0.0/16"
 function print_help(){
    printf "\n\
    \t usage: $0 options\n\
@@ -14,7 +17,79 @@ function print_help(){
    \t\t -h help menu"
 }
 
-echo "INSTALLER SCRIPT FOR K8S LAB RUN IT WITH OPTIONS MASTER WORKER"
+function inst_general_deps(){
+  apt update -y
+  apt install -y apt-transport-https ca-certificates curl
+}
+
+function add_net_modules(){
+  cat <<EOF | tee etc/modules-load.d/k8s.conf
+  overlay
+  br_netfilter
+EOF
+
+  cat <<EOF | tee /etc/sysctl.d/k8s.conf
+  net.bridge.bridge-nf-call-iptables  = 1
+  net.bridge.bridge-nf-call-ip6tables = 1
+  net.ipv4.ip_forward                 = 1
+EOF
+
+  modprobe -a overlay br_netfilter
+  sysctl --system
+}
+
+function install_crio(){
+  ub_v=$(lsb_release -a | grep Release | awk '{print $2}')
+  crio_ver=$CRIO_V
+  OS=xUbuntu_$ub_v
+  echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" > /etc/apt/sources.list.d/devel-libcontainers.list
+  echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$crio_ver/$OS/ /" > /etc/apt/sources.list.d/devel-cri-o-$crio_ver.list 
+  curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$crio_ver/$OS/Release.key | apt-key add -
+  curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | apt-key add -
+  
+  apt update -y
+  apt-get install -y cri-o cri-o-runc
+
+  systemctl daemon-reload
+  systemctl enable crio
+  systemctl start crio
+
+  if [ -S "/var/run/crio/crio.sock" ]; then
+    echo "CRIO-O install suscessfully"
+  else
+    echo "There was soemthing wrong installing cri-o, the socket did not exists on the OS"
+    exit 1
+  fi
+}
+
+function kubeadm_install(){
+  curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+  echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+  apt update -y
+  apt install -y kubelet kubeadm kubectl
+  apt-mark hold kubelet kubeadm kubectl
+}
+
+function swap_handler(){
+  swapoff -a
+  sed -i -e '/swap/d' /etc/fstab
+}
+
+function common_steps(){
+  inst_general_deps
+  add_net_modules
+  install_crio
+  kubeadm_install
+  swap_handler
+}
+
+if [[ "$USERID" != "0" ]]; then
+  echo "This Script needs to be execute as root"
+  exit 1
+fi
+
+
+echo "INSTALLER SCRIPT FOR K8S LAB RUN IT WITH OPTIONS MASTER or WORKER"
 
 while getopts "k:i:d:h" options; do
   case "${options}" in
@@ -22,7 +97,12 @@ while getopts "k:i:d:h" options; do
       print_help
     ;;
     k)
-      KIND=$OPTARG
+      if [[ "$OPTARG" == "MASTER" ]] || [[ "$OPTARG" == "WORKER" ]]; then 
+        KIND=$OPTARG
+      else
+        echo "Option can't be recognized"
+        print_help
+      fi
     ;;
     i)
       if [[ "$KIND" == "MASTER" ]]; then
@@ -44,3 +124,4 @@ while getopts "k:i:d:h" options; do
 done
 echo "Kind $KIND"
 echo "ID $ID"
+common_steps
