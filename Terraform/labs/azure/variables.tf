1e4bd5aba991d25b82dbba431dcee9a2e275137b
Carlos Diaz <cadgo.root@gmail.com>
2024-03-05 14:45:02 -0600

    variables update

--- Terraform/labs/azure/variables.tf
+++ Terraform/labs/azure/variables.tf
@@ -19,3 +19,33 @@ variable "rg_vnet2"{
   description = "resource group for vnet2"
   default = "rg_vnet2"
 }
+
+variable "vnet1_definition"{
+  type = map
+  description = "vnet1 definition"
+  default ={ 
+    name = "vnet1"
+    resource_g = "rg_vnet1"
+    cidr = "10.0.1.0/24"
+  }
+}
+
+variable "vnet2_definition"{
+  type = map
+  description = "vnet2 definition"
+  default = {
+    name = "vnet2"
+    resource_g = "rg_vnet2"
+    cidr = "10.0.2.0/24"
+  }
+}
+
+variable "vnet3_in_serv_fw"{
+  type = map
+  description = "vnet3 definition"
+  default = {
+    name = "vnet3"
+    resource_g = "rg_vnet3"
+    cidr = "10.0.3.0/24"
+  }
+}
